fd0686ca36c604a71e32498df54ced7de37d5def
johntrehearn <johntrehearnbc@gmail.com>
2023-11-14 20:48:24 +0200

    Buttons and likes working

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,63 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import Card from './Card';
 /* import Header from './Header';
 import Footer from './Footer'; */
 import {animals} from './animalsList';
 
 function App() {
-  /* const [animals, setAnimals] = useState([
-    { id: 1, name: 'Cat', type : 'Furry', img: 'https://s3.amazonaws.com/freecodecamp/running-cats.jpg' },
-    { id: 2, name: 'Lion', type : 'Angry' },
-    { id: 3, name: 'Dog', type : 'Hungry' },
-  ]);
- */
-
-
-  // This is the animal list (first item is where the the animals are from then it sets animals and uses the animals in usestate) This uses the import above import {animals} from './animalsList';
   const [animalList, setAnimals] = useState(animals);
+  const [search, setSearch] = useState('');
 
+  const searchHandler = (event) => {
+    setSearch(event.target.value);
+  };
 
+  
 
   const removeHandler = (id) => {
-
-    // id in this can be called anything it is not relevent to anthing else
-    // need to bring in the onclick event from the card.jsx so the button works
-    const updatedAnimalArray = animalList.filter((animal) => animal.name !== id);
+    const updatedAnimalArray = animalList.filter(
+      (animal) => animal.name !== id
+    );
     setAnimals(updatedAnimalArray);
+  };
+
+  const likesHandler = (name, action) => {
+    const updatedArray = animalList.map((animal) => {
+      if (animal.name === name) {
+        if (action === 'add') {
+          return {...animal, likes: animal.likes + 1};
+        } else {
+          return {...animal, likes: animal.likes - 1};
+        }
+      } else {
+        return animal;
+      }
+    });
+    setAnimals(updatedArray);
   };
 
   return (
     <>
       <main>
-        <h1>John's Zoo</h1>
-        <div className="cards">
-          {animalList.map((animal) => (
-// look at the data in animalsList.js you need to use a datapoint from there
-            <Card
-              key={animal.name}
-              {...animal}
-// this shows what to run when the button is clicked
-              click={() => removeHandler(animal.name)}
-            />
-          ))}
+        <h1>John Zoo</h1>
 
-          <Card
-            name={animals[0].name}
-            type={animals[0].type}
-            img={animals[0].img}
-          />
+        <input type="text" onChange={searchHandler} />
+        <div className="cards">
+          {animalList
+            .filter((fox) =>
+              fox.name.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((animal) => (
+              // look at the data in animalsList.js you need to use a datapoint from there
+              <Card
+               addLikes={() => likesHandler(animal.name, 'add')}
+              clearLikes={() => likesHandler(animal.name, 'clear')}
+         /*        fox='Margit' */
+                key={animal.name}
+                {...animal}
+                click={() => removeHandler(animal.name)}
+              />
+            ))}
         </div>
       </main>
       {/* <Footer copyright="Copyright Info"/> */}
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,18 +1,33 @@
 const Card = (props) => {
-    return (
-        <div className='card'>
-           {/* THIS IS THE ONCLICK EVENT TO BRING IN THE BUTTON */}
-            <button onClick = {props.click}className="material-symbols-outlined">close</button>
-            <p id='cardName'>Name: {props.name}</p>
-            <p id='cardType'>Type: {props.type}</p>
-            <img src={`https://source.unsplash.com/400x400/?${props.name}`} alt="picture"  />
-            <p id='cardLikes'><span className="material-symbols-outlined">thumb_up</span>
-            Placeholder for likes
-            <span className="material-symbols-outlined">thumb_down</span></p>
-
+  return (
+    <div className="card">
+      {/* THIS IS THE ONCLICK EVENT TO BRING IN THE BUTTON */}
+      <button onClick={props.click} className="material-symbols-outlined">
+        close
+      </button>
+      <p id="cardName">Name: {props.name}</p>
+      <img
+        src={`https://source.unsplash.com/400x400/?${props.name}`}
+        alt="picture"
+      />
+      <div id="cardLikes">
+        <button onClick={props.addLikes}>
+          <span className="material-symbols-outlined">sentiment_satisfied</span>
+        </button>
+        
+        {/* This is to change the heart to broken */}
+        <div className='likes_box'>
+        <p className="material-symbols-outlined">{props.likes >= 0 ? 'favorite' : 'heart_broken'}</p>
+        <p className="likes">{props.likes}</p>
         </div>
-    )
-}
+
+
+        <button onClick={props.clearLikes}>
+          <span className="material-symbols-outlined">sentiment_dissatisfied</span>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
-
