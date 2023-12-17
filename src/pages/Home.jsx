286c6c6663003e7f50dc17d3d57ecbdfa81b0133
johntrehearn <johntrehearnbc@gmail.com>
2023-12-17 15:41:04 +0200

    Home page completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,36 @@
 import { NavLink } from "react-router-dom";
 import kelpie from '../assets/kelpie.jpg'
 import bird from '../assets/bird.jpg'
+import '../styles/home.css'
+
 
 function Home() {
     return (
         <>
-        <h1> PICTURE TO LINK TO ANIMALS AND BIRDS</h1>
+        <div className='homeBody'>
 
-        <NavLink to="/animals">
-            <img className='entryPic animalsPic' src={kelpie} alt="A picture of a dog linking to animal zoo" />
+            <h1> Welcome to the JT Zoo</h1>
+            <h2>Do you want to visit our animal or birds section?</h2>
+
+            <div className='jtZoos'>
+
+
+            <NavLink to="/animals"> 
+                <img className='entryPic animalsPic' src={kelpie} alt="A picture of a dog linking to animal zoo" />
+            <div className='title_text'>
+
+                <h2>Aminals</h2>
+            </div>
+                </NavLink>
+            <NavLink to="/birds">
+            <img className='entryPic birdPic'src={bird} alt="A picture of a bird linking to bird zoo" />
+            <div className='title_text'>
+
+            <h2>Birds</h2>
+            </div>
             </NavLink>
-        <NavLink to="/birds">Birds</NavLink>
-        <img className='entryPic birdPic'src={bird} alt="A picture of a bird linking to bird zoo" />
+            </div>
+        </div>
         </>
     );
 }
