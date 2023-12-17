74ecbed31a201c08ff6df9d87bec3e18652e74b3
johntrehearn <johntrehearnbc@gmail.com>
2023-12-17 20:40:13 +0200

    Updated header and css on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,26 @@
+import jt from '../assets/forest_path.jpg'
+import '../styles/about.css'
+
 function About () {
     return (
         <>
+
+        <div className='aboutMain'>
+
         <h1>Info about this project</h1>
+
+        <div className='JT_Info'>
+
+
+        <img src={jt} alt="" />
+        <p className='info_text'>This is a project for the React Course at Business College Helsinki.  Please find me at
+        
+        <a href="https://github.com/johntrehearn"> https://github.com/johntrehearn</a>
+        </p>
+
+        </div>
+        </div>
+     
         </>
     )
 }
