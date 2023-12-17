aad77fb1c75316ac7e9847c13697bf4a9cdb81e7
johntrehearn <johntrehearnbc@gmail.com>
2023-12-17 07:00:59 +0200

    css updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,30 +1,30 @@
 const Card = (props) => {
   return (
     <div className="card">
-      {/* THIS IS THE ONCLICK EVENT TO BRING IN THE BUTTON */}
-      <button onClick={props.click} className="material-symbols-outlined">
+      <button onClick={props.click} className="material-symbols-outlined closeBut">
         close
       </button>
-      <p id="cardName">Name: {props.name}</p>
+      <p id="cardName">{props.name.toUpperCase()}</p>
       <img
         src={`https://source.unsplash.com/400x400/?${props.name}`}
         alt="picture"
       />
+      <div>
+
       <div id="cardLikes">
-        <button onClick={props.addLikes}>
-          <span className="material-symbols-outlined">sentiment_satisfied</span>
-        </button>
-        
-        {/* This is to change the heart to broken */}
-        <div className='likes_box'>
-        <p className="material-symbols-outlined">{props.likes >= 0 ? 'favorite' : 'heart_broken'}</p>
-        <p className="likes">{props.likes}</p>
-        </div>
+          <button onClick={props.addLikes}>
+              <span className="material-symbols-outlined">sentiment_satisfied</span>
+          </button>
+          <div className='likes_box'>
+              <p className="material-symbols-outlined">{props.likes >= 0 ? 'favorite' : 'heart_broken'}</p>
+              <p className="likes">{props.likes}</p>
+          </div>
 
 
-        <button onClick={props.clearLikes}>
-          <span className="material-symbols-outlined">sentiment_dissatisfied</span>
-        </button>
+          <button onClick={props.clearLikes}>
+            <span className="material-symbols-outlined">sentiment_dissatisfied</span>
+          </button>
+      </div>
       </div>
     </div>
   );
