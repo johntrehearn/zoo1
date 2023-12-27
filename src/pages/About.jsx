5c0e33135d96107262aeb2bcdb1b6098e404bad8
johntrehearn <johntrehearnbc@gmail.com>
2023-12-27 13:16:54 +0200

    Updated the About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,6 +16,13 @@ function About () {
         <p className='info_text'>This is a project for the React Course at Business College Helsinki.  Please find me at
         
         <a href="https://github.com/johntrehearn"> https://github.com/johntrehearn</a>
+
+        <br/> 
+        <br/> This React Project has search features for either animals and birds, routing for the SPA. Maping to display the API data.
+        <br/> 
+        <br/> 
+
+        I have learned a great deal about useState, rooting, app deployment and maping on this project.
         </p>
 
         </div>
